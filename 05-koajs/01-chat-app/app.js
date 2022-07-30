4f27b236440a98fc5bde2933d614aa4809384090
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2022-07-30 17:37:46 +0300

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,37 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+  const id = Math.random();
+  subscribers[id] = '';
+
+  ctx.set('Content-Type', 'text/plain;charset=utf-8');
+  ctx.set('Cache-Control', 'no-cache, must-revalidate');
+
+  const message = await new Promise(async (resolve) => {
+    const timerId = setInterval(() => {
+      if (subscribers[id]) {
+        resolve(subscribers[id]);
+        clearInterval(timerId);
+      }
+    }, 400);
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  if (message) {
+    Object.keys(subscribers).forEach((id) => {
+      subscribers[id] = message;
+    });
+
+    ctx.body = 'Success!';
+  }
 });
 
 app.use(router.routes());
