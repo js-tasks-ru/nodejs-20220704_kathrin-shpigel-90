c7cd44fc6d5f4e4b71f00cbc518d7a7cf75e151c
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2022-07-31 13:34:37 +0300

    fix solution for 6-module 1-task

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -21,7 +21,6 @@ const productSchema = new mongoose.Schema({
   },
   subcategory: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'SubCategory',
     required: true,
   },
   images: [String],
