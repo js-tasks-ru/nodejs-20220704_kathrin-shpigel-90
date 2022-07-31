772d48fbe71a019a9499252722065f4514f13f67
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2022-07-31 13:25:19 +0300

    add solution for 6-module 2-task

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,33 @@
+const Category = require('../models/Category');
+
+/**
+ * For preparing category for a query response
+ * @param {categorySchema} category
+ * @return {{id: string, title: string, subcategories: {id: string, title: string}[]}}
+ */
+function prepareCategory(category) {
+  return {
+    id: category._id.toString(),
+    title: category.title,
+    subcategories: category.subcategories.map(prepareSubCategory),
+  };
+}
+
+/**
+ * For preparing subCategory for a query response
+ * @param {subCategorySchema} subCategory
+ * @return {{id: string, title: string}}
+ */
+function prepareSubCategory(subCategory) {
+  return {
+    id: subCategory._id.toString(),
+    title: subCategory.title,
+  };
+}
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
-};
+  const categoriesDB = await Category.find({});
+  const categories = categoriesDB.map(prepareCategory);
+
+  ctx.body = {categories};
+}
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,72 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+/**
+ * For preparing product for a query response
+ * @param {productSchema} product
+ * @return {
+ *  {
+ *    images: string[],
+ *    price: number,
+ *    description: string,
+ *    id: string,
+ *    title: string,
+ *    category: string,
+ *    subcategories: string,
+ *  }
+ * }
+ */
+function prepareProduct(product) {
+  return {
+    id: product._id.toString(),
+    title: product.title,
+    images: product.images,
+    category: product.category.toString(),
+    subcategory: product.subcategory.toString(),
+    price: product.price,
+    description: product.description,
+  };
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
+  /**
+   * If subcategory is not passed, then return all products
+   */
   if (!subcategory) return next();
 
-  ctx.body = {};
+  if (!mongoose.isValidObjectId(subcategory)) {
+    ctx.throw(400, 'Id not valid');
+  }
+
+  const productsDB = await Product.find({subcategory});
+  const products = productsDB.map(prepareProduct);
+
+  ctx.body = {products};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productsDB = await Product.find({});
+  const products = productsDB.map(prepareProduct);
+
+  ctx.body = {products};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const {id} = ctx.params;
 
+  if (!mongoose.isValidObjectId(id)) {
+    ctx.throw(400, 'Id not valid');
+  }
+
+  const productDB = await Product.findById(id);
+
+  if (!productDB) {
+    ctx.throw(404, 'Product not found');
+  }
+
+  const product = prepareProduct(productDB);
+
+  ctx.body = {product};
+};
